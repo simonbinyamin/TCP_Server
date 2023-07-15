904da9cd903d3dcb6e5d96300f26cb2fe98f93c7
Simon <simonbinyamin@gmail.com>
2023-07-15 10:25:17 +0200

    Add project files.

---
+++ TCP_Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TCPClient
+{
+    static void Main()
+    {
+        try
+        {
+
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+            int port = 7070;
+            TcpClient tcpclient = new TcpClient();
+            tcpclient.Connect(ipaddress, port);
+            tcpclient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+
+
+            NetworkStream stream = tcpclient.GetStream();
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                string message = "Request nr " + (i + 1);
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", message);
+
+
+                data = new byte[1024];
+                int bytes = stream.Read(data, 0, data.Length);
+                string response = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", response);
+            }
+
+
+
+            tcpclient.Close();
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+
+        Console.ReadLine();
+
+
+    }
+}
---
+++ TCP_Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Console;
+class TCPServer
+{
+    static void Main()
+    {
+        TcpListener tcpserver = null;
+        try
+        {
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+            int port = 7070;
+
+
+            tcpserver = new TcpListener(ipaddress, port);
+            tcpserver.Start();
+            WriteLine("Waiting for a connections...");
+
+
+            TcpClient client = tcpserver.AcceptTcpClient();
+            WriteLine("Client connected....");
+
+
+            NetworkStream networkStream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int readBytes;
+            while ((readBytes = networkStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string data = Encoding.ASCII.GetString(buffer, 0, readBytes);
+                WriteLine("GOT IT: {0}", data);
+
+                byte[] response = Encoding.ASCII.GetBytes("I got your msg");
+                networkStream.Write(response, 0, response.Length);
+                WriteLine("Sent the server response");
+            }
+
+            client.Close();
+        }
+        catch (SocketException e)
+        {
+            WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            // Stop listening for connections
+            tcpserver.Stop();
+        }
+
+        WriteLine("Server is stopped.");
+        ReadLine();
+    }
+}
